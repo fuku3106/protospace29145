b7a55067856c06293fc9bbb7771f640571b6c5a5
fuku3106 <satoru.tohno@gmail.com>
2020-12-18 12:44:28 +0900

    指摘箇所修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,10 +26,10 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
+    unless user_signed_in? && current_user.id == @prototype.user_id 
       redirect_to action: :index
-    end
   end
+end
 
   def update
       @prototype = Prototype.find(params[:id])
